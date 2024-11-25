af90d8743e7560f7f03bce516f5755c2724bee41
nguyendamkha <nguyendamkha@gmail.com>
2024-11-25 08:28:02 +0700

    chinh sua ti index va script

---
+++ a-Calorie-Counter/script.js
@@ -0,0 +1,18 @@
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+let isError = false;
+
+//xoá dấu +,- khi nhập số vào input tag có id là budget
+function cleanInputString(str) {
+  const regex = /[+-\s]/g; //escape + để nó chỉ là dấu + bình thường => regex này match với chuỗi "+-"
+  //g là 1 flag - viết tắt của global
+  return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i; //i flag, i là viết tắt của insensitive
+}
